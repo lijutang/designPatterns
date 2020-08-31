5b8066326ded96ee4ab3d1da065212e21fa52550
ljt <742641068@qq.com>
2020-08-31 16:07:17 +0800

    设计模式之－－、创建型模式

---
+++ chuangJianXingMoShi/3_abstractfactory_test.go
@@ -0,0 +1,25 @@
+package chuangJianXingMoShi
+
+
+func getMainAndDetail(factory DAOFactory) {
+	factory.CreateOrderMainDAO().SaveOrderMain()
+	factory.CreateOrderDetailDAO().SaveOrderDetail()
+}
+//
+//func ExampleRdbFactory() {
+//	var factory DAOFactory
+//	factory = &RDBDAOFactory{}
+//	getMainAndDetail(factory)
+//	// Output:
+//	// rdb main save
+//	// rdb detail save
+//}
+//
+//func ExampleXmlFactory() {
+//	var factory DAOFactory
+//	factory = &XMLDAOFactory{}
+//	getMainAndDetail(factory)
+//	// Output:
+//	// xml main save
+//	// xml detail save
+//}
